7ff0a79506228ff4a91960bc9db0db844382d5a3
SleepertySleep <greezle.gg@mail.ru>
2024-08-19 22:10:22 +0300

    feat:some assets added

--- app/lib/pages/weather_page.dart
+++ app/lib/pages/weather_page.dart
@@ -1,5 +1,6 @@
 import 'package:app/services/weather_service.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 import '../models/weather_model.dart';
 
@@ -30,6 +31,30 @@ class _WeatherPageState extends State<WeatherPage> {
     }
   }
 
+  String getWeatherAnima(String? condition) {
+    if (condition == null) return 'no condition';
+
+    switch (condition.toLowerCase()) {
+      case 'clouds':
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'fog':
+        return 'assets/cloud.json';
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        return 'assets/sun_rain.json';
+      case 'tunderstorm':
+        return 'assets/tunder.json';
+      case 'clear':
+        return 'assets/sun.json';
+      default:
+        return 'assets/sun.json';
+    }
+  }
+
   @override
   void initState() {
     super.initState();
@@ -44,7 +69,12 @@ class _WeatherPageState extends State<WeatherPage> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(_weather?.city ?? 'loading...'),
-          Text('${_weather?.temperature.round()}°C')
+
+          Lottie.asset(getWeatherAnima(_weather?.condition)),
+
+          Text('${_weather?.temperature.round()}°C'),
+
+          Text(_weather?.condition ?? ''),
         ],
       ),
     );
